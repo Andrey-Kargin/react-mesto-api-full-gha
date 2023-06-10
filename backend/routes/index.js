9e2f9d86fa1558989f5b48c8db732a980161abb4
Andrey Kargin <andreykargin0407@gmail.com>
2023-06-10 14:07:13 +0300

    backend added

---
+++ backend/routes/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+
+const router = require('express').Router();
+
+const usersRouter = require('./users');
+const cardsRouter = require('./cards');
+const NotFoundError = require('../errors/not-fount-error');
+
+router.use('/users', usersRouter);
+router.use('/cards', cardsRouter);
+router.use((req, res, next) => {
+  next(new NotFoundError('Страница не найдена'));
+});
+router.use(express.json());
+
+module.exports = router;
